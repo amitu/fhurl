b929e196ec143fbb354699eb932e13fd941af88d
Amit Upadhyay <comments@amitu.com>
2011-03-28 16:47:22 -0400

    release 0.1.1

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fhurl",
-    version = "0.1.0",
-    url = 'http://github.com/amitu/fhurl',
+    version = "0.1.1",
+    url = 'http://packages.python.org/fhurl/',
     license = 'BSD',
     description = "Django generic form handler view",
-    long_description = file("README.rst").read(),
     author = 'Amit Upadhyay',
     author_email = "upadhyay@gmail.com",
     py_modules = ["fhurl"],
