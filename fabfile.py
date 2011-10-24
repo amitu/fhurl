6187c1b85e60255b63558195b5cfa55e869ec333
Amit Upadhyay <comments@amitu.com>
2011-10-24 04:08:18 -0400

    updated docs

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,7 @@ from fabric.api import local
 def docs():
     local("./bin/docs")
     local("./bin/python setup.py upload_sphinx --upload-dir=docs/html")
+
+def release():
+    # update version id in setup.py, changelog and docs/source/conf.py
+    local("python setup.py sdist --formats=gztar,zip upload")
